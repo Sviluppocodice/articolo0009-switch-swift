09ad97a00d122d18425eef305582f8b77579c9a8
SviluppoCodice <51750512+Sviluppocodice@users.noreply.github.com>
2019-07-23 14:56:07 +0200

    Add files via upload

---
+++ Switch_Swift/Switch.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var numeroDaControllare : Int = 3
+
+switch numeroDaControllare {
+case 1:
+    print("il numero è 1")
+    print("altro ")
+    print("codice ")
+case 2, 3:
+    print("il numero è 2 oppure 3")
+default:
+    print("il numero è diverso da 1 e 2")
+}
+
+
